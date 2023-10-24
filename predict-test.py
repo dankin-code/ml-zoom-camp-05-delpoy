2464304caa8e7aed91e959613b9f4b4b7abfb6f3
Dan <dkinai@hotmail.com>
2023-10-24 00:33:37 -0400

    flask app for predictions

---
+++ predict-test.py
@@ -0,0 +1,26 @@
+import requests
+
+
+customer_id = 'xyz_123'
+customer = {
+    'gender': 'female',
+    'seniorcitizen': 0,
+    'partner': 'yes',
+    'dependents': 'no',
+    'phoneservice': 'no',
+    'multiplelines': 'no_phone_service',
+    'internetservice': 'dsl',
+    'onlinesecurity': 'no',
+    'onlinebackup': 'yes',
+    'deviceprotection': 'no',
+    'techsupport': 'no',
+    'streamingtv': 'no',
+    'streamingmovies': 'no',
+    'contract': 'month-to-month',
+    'paperlessbilling': 'yes',
+    'paymentmethod': 'electronic_check',
+    'tenure': 24,
+    'monthlycharges': 29.85,
+    'totalcharges': 24 * 29.85
+}
+
